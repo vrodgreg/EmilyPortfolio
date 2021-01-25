7bb214acb41099b075488350cf78ad124b56cb1e
vrodgreg <75463166+vrodgreg@users.noreply.github.com>
2021-01-24 23:56:11 -0500

    Final submit before presentation.

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
-const test = document.querySelector('.leftHandProject')
-test.href='creations.html'
+/*const test = document.querySelector('.leftHandProject')
+test.href='creations.html'*/
+
+document.getElementById('project1').onclick = function() {
+
+	window.location.href = "creations.html";
+
+}
+
+document.getElementById('project2').onclick = function() {
+
+	window.location.href = "creations.html";
+
+}
+
+document.getElementById('project3').onclick = function() {
+
+	window.location.href = "creations.html";
+
+}
+
+document.getElementById('project4').onclick = function() {
+
+	window.location.href = "creations.html";
+
+}
+
+document.getElementById("project1").onmouseover = function() {mouseOver()};
+
+function mouseOver() {
+	document.getElementById("project1").style.cursor = "pointer";
+}
+
+/*document.getElementById("project2").onmouseover = function() {mouseOver()};
+
+function mouseOver() {
+	document.getElementById("project2").style.cursor = "pointer";
+}
+
+document.getElementById("project3").onmouseover = function() {mouseOver()};
+
+function mouseOver() {
+	document.getElementById("project3").style.cursor = "pointer";
+}
+
+document.getElementById("project4").onmouseover = function() {mouseOver()};
+
+function mouseOver() {
+	document.getElementById("project4").style.cursor = "pointer";
+}*/
